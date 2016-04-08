91c14775ee6bd64d3be25f6071d47c1215265d9a
Machine3 <machine3@Admins-Mac-mini-2.local>
2016-04-08 12:11:10 +0530

    deleted detail page

--- app/src/main/java/com/example/machine2/ne/MainActivity.java
+++ app/src/main/java/com/example/machine2/ne/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.machine2.ne;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity
     Bundle bundle;
     String name;
     ArrayAdapter<String> arrayAdapter;
+//    String filename = "mySharedString";
+//    SharedPreferences someData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +32,30 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         listView=(ListView)findViewById(R.id.listView);
+//        someData = getSharedPreferences(filename,0);
 
         //exception has to be handled when the main activity is first launched
         try {
             // Getting the data from previous activity and showing in list view.
             bundle = getIntent().getExtras();
             name = bundle.getString("mylist");
+//            SharedPreferences.Editor  editor = someData.edit();
+//            editor.putString("sharedString", name);
+//            editor.commit();
             filelist.add(name);
             arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, filelist);
             listView.setAdapter(arrayAdapter);
         }
 
         catch (Exception e) { }
-
-
+//        try {
+//            someData = getSharedPreferences(filename, 0);
+//            String dataReturned = someData.getString("sharedString", "couldn't load data");
+//            filelist.add(dataReturned);
+//            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, filelist);
+//            listView.setAdapter(arrayAdapter);
+//        }
+//        catch (Exception e) {}
         //when clicking the particular item in listview ,pass the data to detailPage activity.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
