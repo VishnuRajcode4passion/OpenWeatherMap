ba32a4c1be735e52f967db5d2b41dc30718882f8
Machine3 <machine3@Admins-Mac-mini-2.local>
2016-04-08 17:32:26 +0530

    edited

--- app/src/main/java/com/example/machine2/ne/MainActivity.java
+++ app/src/main/java/com/example/machine2/ne/MainActivity.java
@@ -14,6 +14,10 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 //Main activity class
 public class MainActivity extends AppCompatActivity
 {
@@ -22,46 +26,52 @@ public class MainActivity extends AppCompatActivity
     Bundle bundle;
     String name;
     ArrayAdapter<String> arrayAdapter;
-//    String filename = "mySharedString";
-//    SharedPreferences someData;
+    String filename = "mySharedString";
+    SharedPreferences someData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        someData = getSharedPreferences(filename,0);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         listView=(ListView)findViewById(R.id.listView);
-//        someData = getSharedPreferences(filename,0);
-
         //exception has to be handled when the main activity is first launched
         try {
             // Getting the data from previous activity and showing in list view.
-            bundle = getIntent().getExtras();
-            name = bundle.getString("mylist");
-//            SharedPreferences.Editor  editor = someData.edit();
-//            editor.putString("sharedString", name);
-//            editor.commit();
-            filelist.add(name);
-            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, filelist);
+            Set<String> set = someData.getStringSet("yourKey", null);
+            List<String> sample=new ArrayList<String>(set);
+     //       filelist.add(dataReturned);
+            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,sample);
             listView.setAdapter(arrayAdapter);
+
         }
 
-        catch (Exception e) { }
-//        try {
-//            someData = getSharedPreferences(filename, 0);
-//            String dataReturned = someData.getString("sharedString", "couldn't load data");
-//            filelist.add(dataReturned);
-//            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, filelist);
-//            listView.setAdapter(arrayAdapter);
-//        }
-//        catch (Exception e) {}
+        catch (Exception e) {
+        }
+
+        try {
+            bundle = getIntent().getExtras();
+            name = bundle.getString("mylist");
+            filelist.add(name);
+            SharedPreferences.Editor  editor = someData.edit();
+            Set<String> set = new HashSet<String>();
+            set.addAll(filelist);
+            editor.putStringSet("yourKey", set);
+            editor.commit();
+//                        arrayAdapter.add();
+            arrayAdapter.notifyDataSetChanged();
+            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,filelist);
+            listView.setAdapter(arrayAdapter);
+        }
+        catch (Exception e) {}
+
         //when clicking the particular item in listview ,pass the data to detailPage activity.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String data=(String)parent.getItemAtPosition(position);
-              //  Intent i = new Intent(MainActivity.this, DetailPage.class);
                 Intent i = new Intent(MainActivity.this,SlidingMainActivity.class);
                 i.putExtra("data",data);
                 startActivity(i);
